cb4eacc14c4e717f8b4013d76892fdbee8749268
Wesley Arcanjo <wesleyferreirarcanjo@gmail.com>
2021-03-02 12:56:31 -0300

    finalizado

---
+++ src/main/java/br/com/chatunipaps/chat_unip/ChatUnipApplication.java
@@ -0,0 +1,13 @@
+package br.com.chatunipaps.chat_unip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatUnipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatUnipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/chatunipaps/chat_unip/ChatUnipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chatunipaps.chat_unip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatUnipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
